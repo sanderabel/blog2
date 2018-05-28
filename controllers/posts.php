7e6c756928fd3e195616757316686ec176f3f148
oksander <39629995+oksander@users.noreply.github.com>
2018-05-28 23:14:54 +0300

    Add single post viewing functionality

--- controllers/posts.php
+++ controllers/posts.php
@@ -16,6 +16,11 @@ class posts extends Controller
         $this->users = get_all("SELECT * FROM post");
     }
 
+    function view() {
+        $post_id = $this->params[0];
+        $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
+    }
+
     /**
      * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
      */
---
+++ views/posts/posts_view.php
@@ -0,0 +1,17 @@
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<?
+foreach ($posts as $post);
+?>
+<div class="span8">
+    <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><h1><?=$post['post_subject']?></h1></a>
+    <p><?=$post['post_text']?></p>
+    <div>
+        <span class="badge badge-success">Posted <?=$post['post_create']?></span><div class="pull-right"><span class="label"><?=$post['user_id']?></span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+    </div>
+    <hr>
+</div>
+<?endforeach?>
